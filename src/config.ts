5fac889024c159b4aed119451196c3e708389ef5
Faizan <faizanms025347@gmail.com>
2025-01-28 20:36:18 +0530

    updates

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = 'http://localhost:5000/api'
+// export const BACKEND_URL = 'http://localhost:5000/api'
+export const BACKEND_URL = 'http://blog-app-backend.up.railway.app/api'
